0c8976244b5b24f3e2e7bdb1e34b758cd6a4b456
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-10-31 09:22:41 +0700

    code build

--- image-search/src/App.js
+++ image-search/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function Loader ()  {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
 
 function App() {
+ 
+const [imageUrls, setImageUrls] = useState([]);
+const [searchText,setSearchText] = useState("");
+const [finalSearch,setFinalSearch] = useState("");
+const [page, setPage] = useState(1);
+const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    if(!finalSearch)
+    return;
+    loadImages();
+  },[finalSearch, page]);
+
+  
+  async function loadImages(){
+    setLoading(true);
+    try {
+    const response = await fetch (`https://api.unsplash.com/search/photos/?query=${searchText}&page=${page}&per_page=30&client_id=LgmeGu6m1WcbZ2VdwgXgdPee5D9Va3PZVe73uc7b3ns`);
+    const imagesResult = await response.json();
+    if (page === 1) {
+      setImageUrls(imagesResult.results.map(image=>image.urls.small));
+    }
+    else {
+   setImageUrls(prev => [...prev,...imagesResult.results.map(image=>image.urls.small)]);
+    }
+  }catch(error){
+    console.log(error);
+  }
+  finally{
+    setLoading(false);
+  }
+  }
+  
+  
+  function handleSearch() {
+    setFinalSearch(searchText);
+    setPage(1);
+    
+  }
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  },[])
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight){
+      setPage(prev => prev + 1);
+    }
+
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><input value={searchText} onChange={(e)=>setSearchText(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+      </div>
+      <div id="gallery">
+       {imageUrls.map((url,index) => <img width="300" height="300" src={url} key={index} alt="" />)}
+       </div>
+       {loading && <Loader />}
     </div>
   );
 }
